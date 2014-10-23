7e18d58f37dc982261ab6313fd9e6d246d035989
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2014-10-23 19:41:12 +0800

    新增 Java 程式語言範本
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

---
+++ Java/Unnamed Java program.java
@@ -0,0 +1,12 @@
+/* 
+	Unnamed Java program.java
+	
+	本來源程式碼的架構基於「通用程式來源程式碼範本」專案
+	This source code's structure is based on "Generic Software Source Code Templates" project
+		https://github.com/Vdragon/Generic_Program_Source_Code_Templates
+		
+	建議的文字編輯器設定
+	Recommended text editor settings
+		Indentation by tab character
+		Tab character width = 2 space characters
+*/
