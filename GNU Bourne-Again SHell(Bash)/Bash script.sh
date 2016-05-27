1d01497a55d44a309b059434ee33b808ab88431f
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2016-05-27 15:12:54 +0800

    GNU Bourne-Again SHell(Bash)/Bash script.sh - 引入 Unofficial Bash Script Mode
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

--- GNU Bourne-Again SHell(Bash)/Bash script.sh
+++ GNU Bourne-Again SHell(Bash)/Bash script.sh
@@ -16,8 +16,16 @@ readonly PROGRAM_DIRECTORY="$(realpath --no-symlinks "$(dirname "$0")")"
 readonly PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
 readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
 
-# 將未定義的變數視為錯誤
+## Unofficial Bash Script Mode
+## http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# 將未定義的變數的參考視為錯誤
 set -u
+
+# Exit immediately if a pipeline, which may consist of a single simple command, a list, or a compound command returns a non-zero status.  The shell does not exit if the command that fails is part of the command list immediately following a `while' or `until' keyword, part of the test in an `if' statement, part of any command executed in a `&&' or `||' list except the command following the final `&&' or `||', any command in a pipeline but the last, or if the command's return status is being inverted with `!'.  If a compound command other than a subshell returns a non-zero status because a command failed while `-e' was being ignored, the shell does not exit.  A trap on `ERR', if set, is executed before the shell exits.
+set -e
+
+# If set, the return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully.
+set -o pipefail
 
 ######## File scope variable definitions ended ########
 
